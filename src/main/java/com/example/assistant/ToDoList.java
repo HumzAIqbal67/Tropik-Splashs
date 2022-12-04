5abb1e2b39c1af07c27a40d852147d4f860ab594
Zaed Ul Islam <zaed.islam@mail.utoronto.ca>
2022-12-03 21:15:55 -0500

    [DEV-1.19]

---
+++ src/main/java/com/example/assistant/ToDoList.java
@@ -0,0 +1,20 @@
+package com.example.assistant;
+
+import java.util.ArrayList;
+
+public class ToDoList {
+    public ArrayList<Task> tasks;
+    public ToDoList(){
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task t){
+        this.tasks.add(t);
+    }
+
+    public void removeTask(Task t){
+        this.tasks.remove(t);
+    }
+
+
+}
